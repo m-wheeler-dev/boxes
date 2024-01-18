c867a20eae4d2575716043feb7b30a45f8dbae21
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-01-17 18:27:49 -0800

    Update for slight rebuild
    Fixed a few code redundancies, updated styling, fixed positioning issues.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const boxesContainer = document.getElementById('boxes');
-const btn = document.getElementById('btn');
+const boxesContainer = document.querySelector("#boxes");
+const btn = document.querySelector("#btn");
 
-btn.addEventListener('click', () => boxesContainer.classList.toggle('big'));
+btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
 
 function makeBoxes() {
-    for (let i = 0; i < 4; i++) {
-        for (let x = 0; x < 4; x++) {
-            const box = document.createElement('div');
-            box.classList.add('box');
-            box.style.backgroundPosition = `${-x * 125}px ${-i * 125}px`;
-            boxesContainer.appendChild(box);
-        }
-        
-    }
+	for (let i = 0; i < 4; i++) {
+		for (let x = 0; x < 4; x++) {
+			const box = document.createElement("div");
+			box.classList.add("box");
+			box.style.backgroundPosition = `${-x * 125}px ${-i * 125}px`;
+			boxesContainer.appendChild(box);
+		}
+	}
 }
 
-makeBoxes()
+makeBoxes();
